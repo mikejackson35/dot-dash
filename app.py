49a20ad572d43f35552ce79686f817d05c369542
Mike_Jackson <mikejackson35@gmail.com>
2023-12-11 11:29:11 -0800

    update

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import plotly.express as px
 
 st.set_page_config(page_title='Dot Sales',
                    page_icon=":bar_chart:",
-                   layout='wide'
+                   layout='centered'
 )
 
 
