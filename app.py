07865ab5962eb7d9efba02d33ae85bafed1e8875
Mike_Jackson <mikejackson35@gmail.com>
2023-12-14 15:30:40 -0800

    upgrade

--- app.py
+++ app.py
@@ -26,35 +26,35 @@ all_sales['Invoice Date'] = all_sales['Invoice Date'].dt.floor('D')
 # --- FILTERS AND SIDEBAR ----
 # variables
 
-# year = st.sidebar.selectbox(
-#     "Year Filter:", all_sales['Invoice Date'].dt.year.unique()
+year = st.sidebar.selectbox(
+    "Year Filter:", all_sales['Invoice Date'].dt.year.unique()
+)
+
+# def options_select():
+#     if "selected_options" in st.session_state:
+#         if -1 in st.session_state["selected_options"]:
+#             st.session_state["selected_options"] = available_options[0]
+#             st.session_state["max_selections"] = 1
+#         else:
+#             st.session_state["max_selections"] = len(available_options)
+
+# available_options = [i for i in range(-1,2)]
+# if "max_selections" not in st.session_state:
+#     st.session_state["max_selections"] = len(available_options)
+
+# st.multiselect(
+#     label="Select an Option",
+#     options=available_options,
+#     key  ="selected_options",
+#     max_selections=st.session_state["max_selections"],
+#     on_change=options_select,
+#     format_func=lambda x: "All" if x == -1 else f"Option {x}",
 # )
 
-def options_select():
-    if "selected_options" in st.session_state:
-        if -1 in st.session_state["selected_options"]:
-            st.session_state["selected_options"] = available_options[0]
-            st.session_state["max_selections"] = 1
-        else:
-            st.session_state["max_selections"] = len(available_options)
-
-available_options = [i for i in range(-1,2)]
-if "max_selections" not in st.session_state:
-    st.session_state["max_selections"] = len(available_options)
-
-st.multiselect(
-    label="Select an Option",
-    options=available_options,
-    key  ="selected_options",
-    max_selections=st.session_state["max_selections"],
-    on_change=options_select,
-    format_func=lambda x: "All" if x == -1 else f"Option {x}",
-)
-
-st.write(
-    available_options[1:] if st.session_state["max_selections"] == 1
-                          else st.session_state["selected_options"]
-)
+# st.write(
+#     available_options[1:] if st.session_state["max_selections"] == 1
+#                           else st.session_state["selected_options"]
+# )
 
 year = st.sidebar.multiselect(
     "Year:",
